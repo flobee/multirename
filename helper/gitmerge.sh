b312f98a02719da03783d55479c4babd78b096b4
flobee <flobee@r61>
2015-08-03 23:14:29 +0200

    improves gitmerge.sh

--- helper/gitmerge.sh
+++ helper/gitmerge.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 ###############################################################################
-# License 
+# License
 #
 # LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
 #
 # @version 1.0.0
 #
 ###############################################################################
-# Note: If you have /bin/dash then change the shebang if /bin/sh does not link 
+# Note: If you have /bin/dash then change the shebang if /bin/sh does not link
 # to it. Alternativly use /bin/ash
 ###############################################################################
 #
-# Helper to merge required externals having the same staging areas: unstable, 
+# Helper to merge required externals having the same staging areas: unstable,
 # testing and master
-# 
+#
 #
 # run: ./helper/gitmerge.sh [branchToMergeIntoTheCurrentOne]
 #
@@ -22,7 +22,7 @@
 # to it and including the externals,
 # run: ./helper/gitupdate.sh [branchname]
 #
-# Important note: 
+# Important note:
 # Staging areas are: "master" "testing" "unstable".
 # "master" should be always the latest stable release!
 # All new code/ development goes to unstable. Also, if needed to the externals.
@@ -31,7 +31,7 @@
 # candidate.
 # Hotfixes will go to extra branches and will be merged directly to the "master"
 #
-# When merging branches take a look into .gitmodules of _each_ branch and 
+# When merging branches take a look into .gitmodules of _each_ branch and
 # verify that the branches still map to the branch name of the master project.
 # E.g.: The branch testing of the project maps to the testing branch of the
 # submodules eg. library and so on.
@@ -49,11 +49,16 @@ do
     if [ "$mybranch" = "$1" ]; then
         echo '+------------------------------------------------------------------------------';
         echo "| check externals, merge $mybranch";
-        git submodule foreach --recursive 'branch="$(git config --file $toplevel/.gitmodules \
-submodule.$name.branch)";\
-echo "| merge external: $name - $mybranch to $branch";\
-cd "$toplevel/$name" && git merge -m "gitmerge.sh $mybranch # to $branch" $mybranch;'
-    
+
+        cmdline="branch=\"\$(git config --file \$toplevel/.gitmodules submodule.\$name.branch)\"; echo \"| merge external: \$name - $mybranch to \$branch\"; cd \"\$toplevel/\$name\" && git merge -m \"gitmerge.sh $mybranch \# to \$branch\" \$mybranch;"
+
+        git submodule foreach --recursive '$cmdline';
+
+#       git submodule foreach --recursive 'branch="$(git config --file $toplevel/.gitmodules \
+#           submodule.$name.branch)";\
+#           echo "| merge external: $name - $mybranch to $branch";\
+#           cd "$toplevel/$name" && git merge -m "gitmerge.sh $mybranch # to $branch" $mybranch;'
+
     else
         echo "| notting to do for $mybranch";
     fi
@@ -65,4 +70,3 @@ echo "\ndone.\n";
 
 
 exit;
-
