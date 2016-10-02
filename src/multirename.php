0984dc1102ae403e82e21f3b6f88cbb1bd99f6cf
flobee <flobee@cygwin>
2016-10-02 20:19:00 +0200

    Improves phpdoc

--- src/multirename.php
+++ src/multirename.php
@@ -4,9 +4,9 @@
  * Multirename
  * for MUMSYS Library for Multi User Management System
  * ----------------------------------------------------------------------------
- * @author Florian Blasel <flobee.code@gmail.com>
- * @copyright (c) 2015 by Florian Blasel
  * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @copyright (c) 2015 by Florian Blasel
+ * @author Florian Blasel <flobee.code@gmail.com>
  * ----------------------------------------------------------------------------
  * @version 1.4.2
  * Created 28.02.2015
--- src/multirename.sh
+++ src/multirename.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
 # /*{{{*/
-# -----------------------------------------------------------------------------
 # Multirename
 # for MUMSYS Library for Multi User Management System
 # -----------------------------------------------------------------------------
-# @author Florian Blasel <flobee.code@gmail.com>
-# -----------------------------------------------------------------------------
-# @copyright (c) 2015 by Florian Blasel
-# -----------------------------------------------------------------------------
 # @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+# @copyright (c) 2015 by Florian Blasel
+# @author Florian Blasel <flobee.code@gmail.com>
 # -----------------------------------------------------------------------------
 # @category    Mumsys
 # @package     Mumsys_Library
