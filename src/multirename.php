e932f66eabff277155e57545668568a084d0775c
flobee <flobee@debian>
2018-03-12 22:17:07 +0100

    Updates file header

--- src/multirename.php
+++ src/multirename.php
@@ -3,11 +3,11 @@
 /**
  * Multirename
  * for MUMSYS Library for Multi User Management System
- * ----------------------------------------------------------------------------
+ *
  * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
  * @copyright (c) 2015 by Florian Blasel
  * @author Florian Blasel <flobee.code@gmail.com>
- * ----------------------------------------------------------------------------
+ *
  * @category    Mumsys
  * @package     Library
  * @subpackage  Multirename
