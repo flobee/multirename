c38f301cac25e7db0908eacd999cb3ce6a993774
flobee <flobee@debian.local>
2023-11-02 22:52:11 +0100

    Fixes error_reporting value php > 5.6

--- src/multirename.php
+++ src/multirename.php
@@ -26,7 +26,7 @@ $pathLibrary = __DIR__ . '/library/mumsys';
 $pathLogfile = '/tmp/';
 
 // --- misc -------------------------------------------------------------------
-error_reporting(-1);
+error_reporting(E_ALL);
 ini_set('display_errors', true);
 ignore_user_abort(false);
 
